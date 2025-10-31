3bf0fcfe8a1778c82ff3b78110483dbefc63de14
Yousaf Zai <yusafzai2007@gmail.com>
2025-10-31 14:08:20 +0500

    fix: Update base URL for tourism API service

--- src/app/services/tourism.ts
+++ src/app/services/tourism.ts
@@ -13,7 +13,7 @@ import { TourismApiResponse } from '../Interface/order';
   providedIn: 'root',
 })
 export class Tourism {
-private baseUrl = 'https://tourism-phyibya04-yusafzai2007s-projects.vercel.app/api/v1/tourism';
+  private baseUrl = 'https://tourism-1g0vf2k6r-yusafzai2007s-projects.vercel.app/api/v1/tourism';
 
   constructor(private http: HttpClient) {}
 
